f32b18e9626e2cb05307b255af7feb5ad21ad4a0
franciscoblancojn <blancofrancisco34@gmail.com>
2025-03-11 14:15:54 -0500

    test api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -7,7 +7,7 @@ export default (req, res) => {
   if (req.method === 'POST') {
     cacheData.put(KEY_CACHE, req.body);
     res.statusCode = 200
-    res.json({ save:"ok" })
+    res.json({ save:"ok", body:req.body })
   } 
   const cache = cacheData.get(KEY_CACHE);
   res.statusCode = 200
