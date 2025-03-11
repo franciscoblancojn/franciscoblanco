fbc70c6bdd4ae43880cd72c8f1618bf06a595b5b
franciscoblancojn <blancofrancisco34@gmail.com>
2025-03-11 14:13:55 -0500

    test api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import cacheData from 'memory-cache';
+
+const KEY_CACHE = "KEY_CACHE"
 
 export default (req, res) => {
+  if (req.method === 'POST') {
+    cacheData.put(KEY_CACHE, req.body);
+    res.statusCode = 200
+    res.json({ save:"ok" })
+  } 
+  const cache = cacheData.get(KEY_CACHE);
   res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.json({ cache })
 }
