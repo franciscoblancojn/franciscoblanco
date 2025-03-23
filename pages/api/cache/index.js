a8d52e10dd09654c839bdf151759664efc56c15e
franciscoblancojn <blancofrancisco34@gmail.com>
2025-03-23 10:13:50 -0500

    change cache

--- pages/api/cache/index.js
+++ pages/api/cache/index.js
@@ -2,15 +2,16 @@
 import cacheData from 'memory-cache';
 
 
+const KEY_CACHE = "KEY_CACHE"
 export default (req, res) => {
   
   if (req.method === 'POST') {
-    cacheData.put(req?.query?.id, req.body,1000000000000);
+    cacheData.put(req?.query?.id ?? KEY_CACHE, req.body,1000000000000);
     res.statusCode = 200
     res.json({ save:"ok", body:req.body })
     return;
   } 
-  const cache = cacheData.get(req?.query?.id);
+  const cache = cacheData.get(req?.query?.id ?? KEY_CACHE);
   res.statusCode = 200
   res.json({ cache })
 }
