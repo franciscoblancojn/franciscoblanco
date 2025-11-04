674c3699b2d05d88dc4260b437383c2db088d688
franciscoblancojn <blancofrancisco34@gmail.com>
2025-11-04 18:34:54 -0500

    access control

--- pages/api/cache/index.js
+++ pages/api/cache/index.js
@@ -8,6 +8,7 @@ export default (req, res) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
+  
   // 🔹 Manejar preflight (OPTIONS)
   if (req.method === "OPTIONS") {
     return res.status(200).end();
