cfc199812558b1b6ba5df40bb2bbd8c8d5854087
franciscoblancojn <blancofrancisco34@gmail.com>
2026-01-08 18:00:48 -0500

    flb

--- components/title.jsx
+++ components/title.jsx
@@ -1,25 +1,30 @@
 import Icon from "@/components/icon";
 
 const Title = ({ children, className = "", back = false }) => {
+  const arrow = `fas ${back ? "fa-caret-left" : "fa-caret-right"}`;
   return (
     <h3
       className={`title ${className}`}
+      style={{
+        viewTransitionName: `${children}`
+          .trim()
+          .toLowerCase()
+          .replace(/\s+/g, "-"),
+      }}
       {...(back
         ? {
             style: {
               cursor: "pointer",
+              viewTransitionName: `${children}`
+                .trim()
+                .toLowerCase()
+                .replace(/\s+/g, "-"),
             },
             onClick: () => window.history.back(),
           }
         : {})}
-      style={{
-        viewTransitionName: `${children}`.trim().toLowerCase().replace(/\s+/g, "-"),
-      }}
     >
-      <Icon marginRight="20px">
-        fas
-        {back ? "fa-caret-left" : "fa-caret-right"}
-      </Icon>
+      <Icon marginRight="20px">{arrow}</Icon>
       {children}
     </h3>
   );
